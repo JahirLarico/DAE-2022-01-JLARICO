b65e1b04ed189a49047bbb918773584566875934
Jahir Larico <laricovargasjahir444@gmail.com>
2022-04-06 18:10:31 -0500

    lab04

--- lab03/lab03/encuesta/views.py
+++ lab03/lab03/encuesta/views.py
@@ -7,22 +7,26 @@ def index(request):
     latest_question_list = Pregunta.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'encuesta/index.html', context)
+
 def detalle(request, pregunta_id):
     pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
     return render(request, 'encuesta/detalle.html', {'pregunta': pregunta})
+
 def votar(request, pregunta_id):
     pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
     try:
-        selected_opcion = pregunta.opcion_set.get(pk=request.POST['opcion'])
+        opcion_id = request.POST['opcion']
+        opcion = Opcion.objects.get(pk=opcion_id)
+        opcion.votos += 1
+        opcion.save()
+        ##redirect to url
+        url='/encuesta/'+str(pregunta_id)+'/'
+        return HttpResponseRedirect(url)
     except (KeyError, Opcion.DoesNotExist):
         return render(request, 'encuesta/detalle.html', {
             'pregunta': pregunta,
-            'error_message': "No se selecciono ninguna opcion",
-        })
-    else:
-        selected_opcion.votos += 1
-        selected_opcion.save()
-        return HttpResponseRedirect(reverse('encuesta:resultados', args=(pregunta.id,)))
+            'error_message': "No se seleccionó ninguna opción",
+        })   
 def resultados(request, pregunta_id):
     pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
     return render(request, 'encuesta/resultados.html', {'pregunta': pregunta})
