3dbab629a8e19c9f4f43e52497abab7b8763f9d7
Jahir Larico <laricovargasjahir444@gmail.com>
2022-04-06 18:29:51 -0500

    lab03

--- lab02/lab02/encuesta/urls.py
+++
@@ -1,9 +0,0 @@
-from pydoc import resolve
-from django.urls import path
-from . import views
-app_name='encuesta'
-
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('enviar',views.enviar,name='enviar'),
-]
