f82a1a5b9c8ec5d3597c187ae90c6b61a8b8e958
Jahir Larico <laricovargasjahir444@gmail.com>
2022-05-27 14:27:20 -0500

    LAB11

---
+++ lab08/lab08/series/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.IndexView.as_view(),name='index'),
+    path('series',views.SeriesView.as_view(),name='series'),
+    path('series/<int:serie_id>',views.SerialDetailView.as_view()),
+]
